194596709a1106f2b6dfc13580fb62b03518a798
abhinav7693ngh <abhinav7693ngh@gmail.com>
2020-02-08 16:22:42 +0530

    error handled

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,10 +2,12 @@ import React,{useState, useEffect, useCallback} from 'react';
 import IngredientList from './IngredientList';
 import IngredientForm from './IngredientForm';
 import Search from './Search';
+import ErrorModel from '../UI/ErrorModal';
 
 const Ingredients = () => {
 
   const [userIngredients, setUserIngredients] = useState([]);
+  const [error, setError] = useState();
 
   useEffect(()=>{
     fetch('https://react-hooks-72979.firebaseio.com/ingredients.json').then(response =>
@@ -21,6 +23,8 @@ const Ingredients = () => {
         setUserIngredients(loadedIngredients);
         console.log(loadedIngredients);
       }
+    }).catch(error => {
+      setError('Something went wrong !!');
     });
   },[]);
 
@@ -40,12 +44,19 @@ const Ingredients = () => {
     }).then(responseData => {
       setUserIngredients(prevIngredients => [...prevIngredients,
       { id: responseData.name, ...ingredient }])
+    }).catch(error => {
+      setError('Something went wrong !!');
     });
     
   }
 
+  const clearError = () => {
+    setError(null);
+  }
+
   return (
     <div className="App">
+      {error && <ErrorModel onClose={clearError}>{error}</ErrorModel>}
       <IngredientForm onAddIngredient={addIngredientHandler}/>
 
       <section>
